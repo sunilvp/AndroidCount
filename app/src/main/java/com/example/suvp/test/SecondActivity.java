f3bb41edc63bb948fbc1595d31ed5b65001c69ae
sunilvp <vip.sunil@gmail.com>
2016-01-06 18:44:06 +0530

    Introducing Second Acitvity and intents

---
+++ app/src/main/java/com/example/suvp/test/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.suvp.test;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+/**
+ * Created by suvp on 1/5/2016.
+ */
+public class SecondActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
